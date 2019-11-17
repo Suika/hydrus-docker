798f035cdb316370b64591d636996502f68f0b68
suika <2320837+Suika@users.noreply.github.com>
2019-11-17 02:49:37 +0100

    Qt upgrade, alpine image
    Move to Qt.

---
+++ client/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+USER_ID=${UID}
+GROUP_ID=${GID}
+
+echo "Starting Hydrus with UID/GID : $USER_ID/$GROUP_ID"
+
+echo "Patching Hydrus"
+
+cd /opt/hydrus/
+patch -f -p1 -i /patch.patch
+
+#if [ $USER_ID !=  0 ] && [ $GROUP_ID != 0 ]; then
+#  find /opt/hydrus/ -not -path "/opt/hydrus/db/*" -exec chown hydrus:hydrus "{}" \;
+#fi
+
+supervisord -c /etc/supervisor.conf
