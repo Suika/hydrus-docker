d15ac0a542d05691733a4555ea830813d15e8d6b
suika <2320837+Suika@users.noreply.github.com>
2019-12-29 17:52:08 +0100

    home path was not set, fixed db path

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -8,7 +8,10 @@ echo "Starting Hydrus with UID/GID : $USER_ID/$GROUP_ID"
 echo "Patching Hydrus"
 
 cd /opt/hydrus/
-patch -f -p1 -i /patch.patch
+
+if [ -f "/patch.patch" ]; then
+  patch -f -p1 -i /patch.patch
+fi
 
 #if [ $USER_ID !=  0 ] && [ $GROUP_ID != 0 ]; then
 #  find /opt/hydrus/ -not -path "/opt/hydrus/db/*" -exec chown hydrus:hydrus "{}" \;
